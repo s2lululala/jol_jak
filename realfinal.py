6a54652192afe5494a922adb9d436be79551885b
soohyun <djm05190@gmail.com>
2021-05-12 06:52:58 +0000

    realfinal

---
+++ realfinal.py
@@ -0,0 +1,52 @@
+import torch
+model = torch.hub.load('pytorch/vision:v0.9.0', 'mobilenet_v2', pretrained=True)
+model.eval()
+
+# img_path = "./turkish_coffee.jpg"
+
+import urllib
+url, filename = ("https://specials-images.forbesimg.com/imageserve/5de43ea0ea103f000653d4cd/960x0.jpg?fit=scale", "dog.jpg")
+try: urllib.URLopener().retrieve(url, filename)
+except: urllib.request.urlretrieve(url, filename)
+
+# sample execution (requires torchvision)
+from PIL import Image
+from torchvision import transforms
+import cv2
+input_image = Image.open(filename)
+#input_image = cv2.imread(img_path)
+
+
+preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+input_tensor = preprocess(input_image)
+input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
+
+# move the input and model to GPU for speed if available
+if torch.cuda.is_available():
+    input_batch = input_batch.to('cuda')
+    model.to('cuda')
+
+with torch.no_grad():
+    output = model(input_batch)
+# Tensor of shape 1000, with confidence scores over Imagenet's 1000 classes
+### not use
+# print(output[0])
+# The output has unnormalized scores. To get probabilities, you can run a softmax on it.
+probabilities = torch.nn.functional.softmax(output[0], dim=0)
+### not use
+# print(probabilities)
+
+# Download ImageNet labels
+
+# Read the categories
+with open("imagenet_classes.txt", "r") as f:
+    categories = [s.strip() for s in f.readlines()]
+# Show top categories per image
+top5_prob, top5_catid = torch.topk(probabilities, 5)
+for i in range(top5_prob.size(0)):
+    print(categories[top5_catid[i]], top5_prob[i].item())
